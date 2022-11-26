846c6082aaeb6a816934597d62479bf106fc7062
Vitaliy Klochko <89vklochko89@gmail.com>
2022-11-26 18:52:52 +0300

    Begin

---
+++ src/main/java/com/example/mockitohw35/MockitoHw35Application.java
@@ -0,0 +1,13 @@
+package com.example.mockitohw35;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoHw35Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoHw35Application.class, args);
+    }
+
+}
